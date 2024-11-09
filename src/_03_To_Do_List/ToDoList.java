982b98c593819e2d2bef8dfe6a28cc5a52b980c4
league <league@imac-26.attlocal.net>
2024-11-08 18:32:27 -0800

    working on to do list

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,11 +1,19 @@
 package _03_To_Do_List;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ToDoList {
 	/*
@@ -61,6 +69,12 @@ public class ToDoList {
 		panel.add(saveButton);
 		panel.add(loadButton);
 		
+		addButton.setText("Add Item");
+		viewButton.setText("View List");
+		removeButton.setText("Remove Item");
+		saveButton.setText("Save List");
+		loadButton.setText("Load List");
+		
 		addButton.addActionListener((arg) -> {
 			String task = JOptionPane.showInputDialog("Add a new task:");
 			if (task.charAt(0) >= 48 && task.charAt(0) <= 57)
@@ -78,40 +92,128 @@ public class ToDoList {
 			}
 			
 			tasks.add(task);
+			JOptionPane.showMessageDialog(null, "Added: " + task);
 		});
+		
 		viewButton.addActionListener((arg) -> {
 			String tasksList = "";
 			for (int i = 0; i < tasks.size(); i++)
 			{
-				tasksList += "\n" + tasks.get(i);
+				tasksList += "\n" + i + " - " + tasks.get(i);
 			}
 			JOptionPane.showMessageDialog(null, "Tasks:" + tasksList);
 		});
+		
 		removeButton.addActionListener((arg) -> {
 			String task = JOptionPane.showInputDialog("Remove a task: Enter a name or number");
 			if (task.charAt(0) >= 48 && task.charAt(0) <= 57)
 			{
 				int id = Integer.parseInt(task);
-				if (id < tasks.size())
+				if (id >= tasks.size() || id < 0)
 				{
+					JOptionPane.showMessageDialog(null, "No task exists with that id");
 					return;
 				}
-				tasks.remove(Integer.parseInt(task));
+				JOptionPane.showMessageDialog(null, "Removed: " + tasks.get(id));
+				tasks.remove(id);
 				return;
 			}
 			else
 			{
 				if (!tasks.remove(task))
 				{
-					
+					JOptionPane.showMessageDialog(null, "No task exists with that name");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Removed: " + task);
 				}
 			}
 		});
-		saveButton.addActionListener((arg) -> {});
-		loadButton.addActionListener((arg) -> {});
 		
+		saveButton.addActionListener((arg) -> {
+			try {
+				String name = JOptionPane.showInputDialog("Enter a name for this list");
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/" + name + ".txt");
+				for (int i = 0; i < tasks.size(); i++)
+				{
+					fw.write((i != 0 ? "/" : "") + tasks.get(i));
+				}
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		
+		loadButton.addActionListener((arg) -> {
+			JFileChooser jfc = new JFileChooser();
+			File currentDir = getDirectoryOfCurrentClass();
+	        if (currentDir != null) {
+	            jfc.setCurrentDirectory(currentDir);
+	        }
+	        //jfc.setFileFilter(new FileNameExtensionFilter("txt"));
+			
+			int returnVal = jfc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				String fileName = jfc.getSelectedFile().getPath();
+				System.out.println(fileName);
+				
+				try {
+					FileReader fr = new FileReader(fileName);
+					
+					tasks.clear();
+					int i = fr.read();
+					String next = "";
+					while (i != -1)
+					{
+						if (i == 47)
+						{
+							tasks.add(next);
+							next = "";
+						}
+						else
+						{
+							next += (char)i;
+						}
+						i = fr.read();
+					}
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+		});
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.pack();
+	}
+
+	
+	
+	private File getDirectoryOfCurrentClass() {
+		 try {
+	            // Get the location of the current class file (compiled class or JAR)
+	            URL location = ToDoList.class.getProtectionDomain().getCodeSource().getLocation();
+	            
+	            // Convert the URL to a File object and return its directory
+	            File binFile = new File(location.toURI());
+	            File file = new File(binFile, "_03_To_Do_List");
+	            if (file.isDirectory()) {
+	                return file;
+	            } else {
+	                return file.getParentFile(); // For JAR files, return the directory containing the JAR
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            return null; // Return null if something goes wrong
+	        }
 	}
 }
